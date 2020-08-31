5b75a9520542d111427ce1ca18f9bdac107ea499
yshir <yshir0020@gmail.com>
2020-09-01 02:08:19 +0900

    add split-phase

---
+++ src/split-phase/after.js
@@ -0,0 +1,16 @@
+const priceOrder = (product, quantity, shippingMethod) => {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+};
+
+const calculatePricingData = (product, quantity) => {
+  const basePrice = product.basePrice * quantity;
+  const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+  return { basePrice, quantity, discount };
+};
+
+const applyShipping = (priceData, shippingMethod) => {
+  const shippingPerCase = priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  return priceData.basePrice - priceData.discount + shippingCost;
+};
