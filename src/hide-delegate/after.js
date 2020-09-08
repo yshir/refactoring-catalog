734222877844bee0ff8a9f52d55750a6c48fabce
yshir <yshir0020@gmail.com>
2020-09-08 18:57:58 +0900

    add hide-delegate

---
+++ src/hide-delegate/after.js
@@ -0,0 +1,38 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+    // ...
+  }
+
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  set department(arg) {
+    this._department = arg;
+  }
+  get manager() {
+    return this._department.manager;
+  }
+}
+
+class Department {
+  // ...
+
+  get chargeCode() {
+    return this._chargeCode;
+  }
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+  get manager() {
+    return this._manager;
+  }
+  set manager(arg) {
+    this._manager = arg;
+  }
+}
+
+const manager = person.manager;
